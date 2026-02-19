ca20b2d8d135e79b8f153770e0513406da11468f
ZainUET <zain41uet@gmail.com>
2026-02-19 16:09:02 +0500

    .

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5656a30e1a2be06be50caa670fa1eb257fee52fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880fd7dd7513b81357876404551d4fa6a3fc4a1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
