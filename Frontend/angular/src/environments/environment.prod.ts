04c0e5b04593025f91a454924e3c2cdc9c450dd1
AbdulAhad87522 <ahadbahi263@gmail.com>
2026-03-14 22:25:38 +0500

    m

--- Frontend/angular/src/environments/environment.prod.ts
+++ Frontend/angular/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7073/api'  // Change this to your production API URL later
+  //apiUrl: 'https://localhost:7073/api'  // Change this to your production API URL later
+  apiUrl: 'hardwarestore-production.up.railway.app'  
+
 };
